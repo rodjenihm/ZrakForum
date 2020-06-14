904d4e8d34ed4d41fb7485e20fd738eb3eb7a157
Данијел Митровић <rodjenihm@outlook.com>
2020-06-15 01:47:29 +0200

    Add uspGetRolesByUserId stored procedure

---
+++ ZrakForum.Database/dbo/Stored Procedures/uspGetRolesByUserId.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[uspGetRolesByUserId]
+	@UserId NVARCHAR(32)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	IF NOT EXISTS (SELECT TOP 1 [Id] FROM [dbo].[Users] WHERE [Id] = @UserId)
+	BEGIN
+		DECLARE @message_text NVARCHAR(MAX) = FORMATMESSAGE('Korisnik čiji je Id %s ne postoji', @UserId)
+		RAISERROR(@message_text, 16, 1)
+	END
+	ELSE
+	BEGIN
+		SELECT r.* FROM [dbo].[UserRoles] ur INNER JOIN [dbo].[Roles] r ON r.Id = ur.RoleId WHERE ur.UserId = @UserId
+	END
+END
