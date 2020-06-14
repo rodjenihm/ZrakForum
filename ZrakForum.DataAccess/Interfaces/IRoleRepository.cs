8d5af854621ce1a8d9055b5f363af4f617a0e3c4
Данијел Митровић <rodjenihm@outlook.com>
2020-06-15 01:47:49 +0200

    Add RoleRepository implementation of IRoleRepository

---
+++ ZrakForum.DataAccess/Interfaces/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ZrakForum.EntityModel;
+
+namespace ZrakForum.DataAccess
+{
+    public interface IRoleRepository
+    {
+        Task<IEnumerable<Role>> GetByUserIdAsync(string userId);
+    }
+}
---
+++ ZrakForum.DataAccess/RoleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using ZrakForum.EntityModel;
+
+namespace ZrakForum.DataAccess
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly ConnectionString connectionString;
+
+        public RoleRepository(ConnectionString connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<Role>> GetByUserIdAsync(string userId)
+        {
+            using var connection = new SqlConnection(connectionString.Value);
+            var sql = "uspGetRolesByUserId @UserId";
+            return await connection.QueryAsync<Role>(sql, new { UserId = userId });
+        }
+    }
+}
