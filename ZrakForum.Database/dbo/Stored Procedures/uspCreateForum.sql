b05bab0eeafa1694782eec037a16c3e7d3c14eb8
Данијел Митровић <rodjenihm@outlook.com>
2020-06-14 19:15:24 +0200

    Add uspCreateForum stored procedure

---
+++ ZrakForum.Database/dbo/Stored Procedures/uspCreateForum.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[uspCreateForum]
+	@Id NVARCHAR(32),
+	@Name NVARCHAR(255),
+	@Description NVARCHAR(MAX)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	IF EXISTS (SELECT TOP 1 [Name] FROM [dbo].[Forums] WHERE [Name] = @Name)
+	BEGIN
+		DECLARE @message_text NVARCHAR(MAX) = FORMATMESSAGE('Forum sa imenom %s već postoji', @Name)
+		RAISERROR(@message_text, 16, 1)
+	END
+	ELSE
+	BEGIN
+		INSERT INTO [dbo].[Forums] ([Id], [Name], [Description]) VALUES (@Id, @Name, @Description)
+	END
+END
