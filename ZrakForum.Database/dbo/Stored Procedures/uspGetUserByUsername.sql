8a7be434e4873abdbd6e804ae46dd71edba42b38
Данијел Митровић <rodjenihm@outlook.com>
2020-06-15 17:05:53 +0200

    Add uspGetUserByUsername stored procedure

---
+++ ZrakForum.Database/dbo/Stored Procedures/uspGetUserByUsername.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[uspGetUserByUsername]
+	@Username NVARCHAR(255)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT [CreatedAt], [Id], [Username], [PasswordHash] FROM [dbo].[Users] WHERE [Username] = @Username
+END
