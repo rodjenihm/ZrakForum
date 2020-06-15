ef7f2f1fae6017af3e648b72e13ba323a3d5c3f1
Данијел Митровић <rodjenihm@outlook.com>
2020-06-15 16:19:18 +0200

    Add UserController and HomeController

---
+++ ZrakForum.Web/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZrakForum.DataAccess;
+using ZrakForum.Web.Dto;
+
+namespace ZrakForum.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserLoginDto model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserRegisterDto model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+
+
+            return View();
+        }
+    }
+}
--- ZrakForum.Web/Startup.cs
+++ ZrakForum.Web/Startup.cs
@@ -1,21 +1,48 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ZrakForum.DataAccess;
+using ZrakForum.Services;
 
 namespace ZrakForum.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = new ConnectionString(Configuration.GetConnectionString("ZrakForum"));
+            services.AddSingleton(connectionString);
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(configureOptions =>
+                {
+                    configureOptions.Cookie.Name = "Cookie.ZrakForum";
+                    configureOptions.LoginPath = "/User/Login";
+                    configureOptions.AccessDeniedPath = "/Home/AccessDenied";
+                });
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +52,40 @@ namespace ZrakForum.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                   name: "login",
+                   pattern: "Login",
+                   defaults: new { controller = "User", action = "Login" });
+
+                endpoints.MapControllerRoute(
+                   name: "register",
+                   pattern: "Register",
+                   defaults: new { controller = "User", action = "Register" });
+
+                endpoints.MapControllerRoute(
+                   name: "logout",
+                   pattern: "Logout",
+                   defaults: new { controller = "User", action = "Logout" });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
