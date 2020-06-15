a8148b6fb5988717db02564352323f686b87e223
Данијел Митровић <rodjenihm@outlook.com>
2020-06-15 22:59:53 +0200

    Add stored procedures to count topics and replies by forum id

---
+++ ZrakForum.Database/dbo/Stored Procedures/uspGetRepliesCountByForumId.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[uspGetRepliesCountByForumId]
+	@ForumId NVARCHAR(32)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	IF NOT EXISTS (SELECT TOP 1 [Id] FROM [dbo].[Forums] WHERE [Id] = @ForumId)
+	BEGIN
+		DECLARE @message_text NVARCHAR(MAX) = FORMATMESSAGE('Forum čiji je Id %s ne postoji', @ForumId);
+		THROW 50000, @message_text, 1
+	END
+	ELSE
+	BEGIN
+		SELECT COUNT([Id]) FROM [dbo].[Replies] WHERE [TopicId] IN
+			(SELECT [Id] FROM [dbo].[Topics] WHERE [ForumId] = @ForumId)
+	END
+END
---
+++ ZrakForum.Database/dbo/Stored Procedures/uspGetTopicsCountByForumId.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[uspGetTopicsCountByForumId]
+	@ForumId NVARCHAR(32)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	IF NOT EXISTS (SELECT TOP 1 [Id] FROM [dbo].[Forums] WHERE [Id] = @ForumId)
+	BEGIN
+		DECLARE @message_text NVARCHAR(MAX) = FORMATMESSAGE('Forum čiji je Id %s ne postoji', @ForumId);
+		THROW 50000, @message_text, 1
+	END
+	ELSE
+	BEGIN
+		SELECT count([Id]) FROM [dbo].[Topics] WHERE ForumId = @ForumId
+	END
+END
