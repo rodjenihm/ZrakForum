3fae2cd57cb6ff436b5f94767fe8df223cef3fc2
Данијел Митровић <rodjenihm@outlook.com>
2020-06-20 18:25:21 +0200

    Add Messages table

---
+++ ZrakForum.Database/dbo/Table/Messages.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Messages]
+(
+	[CreatedAt] DATETIME2(7) NOT NULL DEFAULT GETDATE(),
+	[Id] NVARCHAR(32) NOT NULL PRIMARY KEY,
+	[SenderId] NVARCHAR(32) NOT NULL,
+	[ReceiverId] NVARCHAR(32) NOT NULL, 
+    CONSTRAINT [FK_Messages_Users_Sender] FOREIGN KEY ([SenderId]) REFERENCES [Users]([Id]),
+    CONSTRAINT [FK_Messages_Users_Receiver] FOREIGN KEY ([ReceiverId]) REFERENCES [Users]([Id])
+)
