3690cfb4d4ecfd126697e9453d41768d43ce0733
Данијел Митровић <rodjenihm@outlook.com>
2020-06-20 20:19:09 +0200

    Add Message entity

---
+++ ZrakForum.EntityModel/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ZrakForum.EntityModel
+{
+    public class Message : BaseEntity
+    {
+        [Required]
+        public string Text { get; set; }
+        public string SenderId { get; set; }
+        public User Sender { get; set; }
+        public string ReceiverId { get; set; }
+        public User Receiver { get; set; }
+    }
+}
