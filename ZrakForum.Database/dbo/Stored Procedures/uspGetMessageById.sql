e333da188d0b47d0beed20121dec853dbf7aa59b
Данијел Митровић <rodjenihm@outlook.com>
2020-06-23 00:50:38 +0200

    Add sotred procedures to Get message by id

---
+++ ZrakForum.Database/dbo/Stored Procedures/uspGetMessageById.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[uspGetMessageById]
+	@Id NVARCHAR(32)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	IF NOT EXISTS (SELECT TOP 1 [Id] FROM [dbo].[Messages] WHERE [Id] = @Id)
+	BEGIN
+		DECLARE @message_text NVARCHAR(MAX) = FORMATMESSAGE('Poruka čiji je Id %s ne postoji', @Id);
+		THROW 50000, @message_text, 1
+	END
+	ELSE
+	BEGIN
+		SELECT
+		*
+		FROM [dbo].[Messages]
+		WHERE [Id] = @Id
+	END
+END
